336340df7972a984c3493d298aee6982adffa92a
Connie Shi <connie.shi@nyu.edu>
2014-12-07 11:19:27 -0500

    UI padding

--- app/src/main/java/com/kiraprentice/catpix/MainActivity.java
+++ app/src/main/java/com/kiraprentice/catpix/MainActivity.java
@@ -3,6 +3,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
