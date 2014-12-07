3a9be4c6ee2e2d57b256b584377dcfcb37179322
Robert Gardner <rhg259@nyu.edu>
2014-12-07 00:01:22 -0500

    Reindent ImageAdapter

--- app/src/main/java/com/kiraprentice/catpix/ImageAdapter.java
+++ app/src/main/java/com/kiraprentice/catpix/ImageAdapter.java
@@ -13,33 +13,33 @@ import android.widget.ImageView;
 public class ImageAdapter extends BaseAdapter {
     private Context mContext;
 
-            public int getCount() {
-                return mThumbIds.length;
-            }
+    public int getCount() {
+                        return mThumbIds.length;
+                                                }
 
-            public Object getItem(int position) {
-                return null;
-            }
+    public Object getItem(int position) {
+                                      return null;
+                                                  }
 
-            public long getItemId(int position) {
-                return 0;
-            }
+    public long getItemId(int position) {
+                                      return 0;
+                                               }
 
-            // create a new ImageView for each item referenced by the Adapter
-            public View getView(int position, View convertView, ViewGroup parent) {
-                ImageView imageView;
-                if (convertView == null) {  // if it's not recycled, initialize some attributes
-                    imageView = new ImageView(mContext);
-                    imageView.setLayoutParams(new GridView.LayoutParams(350,350));
-                    imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                    //imageView.setPadding(8, 8, 8, 8);
-                } else {
-                    imageView = (ImageView) convertView;
-                }
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(350,350));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            //imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
 
-                imageView.setImageResource(mThumbIds[position]);
-                return imageView;
-            }
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
 
     // references to our images
     private Integer[] mThumbIds = {
