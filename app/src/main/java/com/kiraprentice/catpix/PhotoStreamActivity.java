66957a5b2c231c7834aa7b25edbd6e4bf88a052e
kiracp <kcp288@nyu.edu>
2014-12-07 04:39:39 -0500

    Added toolbar to homepage with secret button

--- app/src/main/java/com/kiraprentice/catpix/PhotoStreamActivity.java
+++ app/src/main/java/com/kiraprentice/catpix/PhotoStreamActivity.java
@@ -9,40 +9,80 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
-public class PhotoStreamActivity extends Activity {
+public class PhotoStreamActivity extends Activity implements View.OnClickListener {
+
+    private int mActivePointerId;
+    private ImageButton image;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
+        System.out.println("Entered onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_stream);
-
-        ImageButton secretKey = (ImageButton) findViewById(R.id.photoKey);
-
-        secretKey.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(PhotoStreamActivity.this, CipherButtons.class);
-                startActivity(intent);
-            }
-        });
 
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
 
+
+        /*
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 Intent intent = new Intent(PhotoStreamActivity.this, CipherButtons.class);
                 startActivity(intent);
                 Toast.makeText(PhotoStreamActivity.this, "" + position, Toast.LENGTH_SHORT).show();
             }
+
         });
+        */
+
+
+        Button toolbarButton =  (Button) findViewById(R.id.toolbarButton);
+
+        toolbarButton.setOnClickListener(PhotoStreamActivity.this);
 
     }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(PhotoStreamActivity.this, CipherButtons.class);
+        startActivity(intent);
+    }
+
+
+/*
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        Intent intent = new Intent(PhotoStreamActivity.this, CipherButtons.class);
+        startActivity(intent);
+        System.out.println("Doubletap registered");
+        //Toast.makeText(PhotoStreamActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
+    public boolean onTouchEvent(MotionEvent event) {
+        // Get the pointer ID
+        mActivePointerId = event.getPointerId(0);
+
+
+        // Use the pointer ID to find the index of the active pointer
+        // and fetch its position
+        int pointerIndex = event.findPointerIndex(mActivePointerId);
+        // Get the pointer's current position
+        float x = event.getX(pointerIndex);
+        float y = event.getY(pointerIndex);
+    }
+    */
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -50,7 +90,6 @@ public class PhotoStreamActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        System.out.println(id);
 
         if (id == R.id.action_settings) {
             return true;
